64dad01b2f9f96399ce4dae13fb7e4439107dad5
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-07-06 17:11:23 -0300

    Faz uma requisição para buscar o filme que deve ser renderizado dentro de MovieDetails

--- src/App.js
+++ src/App.js
@@ -11,7 +11,11 @@ function App() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={ MovieList } />
-      <Route exact path="/movies/:id" component={ MovieDetails } />
+      <Route
+        path="/movies/:id"
+        render={ (props) => <MovieDetails { ...props } greetingsMessage="Good Morning" /> }
+        // component={ MovieDetails }
+      />
       <Route exact path="/movies/new" component={ NewMovie } />
       <Route exact path="/movies/:id/edit" component={ EditMovie } />
       <Route exact path="/" component={ NotFound } />
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
-import { Loading } from '../components';
+import Loading from '../components/Loading';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      movies: [],
+    };
+  }
+
+  componentDidMount() {
+    this.fetchApiMovies();
+  }
+
+  async fetchApiMovies() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const { match: { params: { id } } } = this.props;
+        const result = await movieAPI.getMovie(id);
+        console.log(result);
+        this.setState({
+          loading: false,
+          movies: result,
+        });
+      },
+    );
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { loading, movies } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movies;
 
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+      <div data-testid="movie-details" className="movie-list">
+        { loading
+          ? <Loading />
+          : <div className="movie-card">
+            <img
+              alt="Movie Cover"
+              src={ `../${imagePath}` }
+              className="movie-card-image"
+            />
+            <h4 className="movie-card-title">{ title }</h4>
+            <p className="movie-card-storyline">{ subtitle }</p>
+            <p className="movie-card-storyline">{ `Storyline: ${storyline}` }</p>
+            <p className="movie-card-storyline">{ `Genre: ${genre}` }</p>
+            <span className="rating">{ `Rating: ${rating}`}</span>
+          </div> }
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;
