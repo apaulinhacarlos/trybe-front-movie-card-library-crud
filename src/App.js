c4394706c103833be37446fdef084a774a73b145
Paulinha Carlos <apaulinhacarlos@gmail.com>
2021-07-06 16:02:21 -0300

    Buscar e mostra a lista de filmes quando MovieList eh montado e exibe o texto Carregando... durante a requisicao

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
+import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={ MovieList } />
-      <Route path="/movies/:id" component={ MovieDetails } />
-      <Route path="/movies/new" component={ NewMovie } />
-      <Route path="/movies/:id/edit" component={ EditMovie } />
-      <Route path="" component={ NotFound } />
+      <Route exact path="/" component={ MovieList } />
+      <Route exact path="/movies/:id" component={ MovieDetails } />
+      <Route exact path="/movies/new" component={ NewMovie } />
+      <Route exact path="/movies/:id/edit" component={ EditMovie } />
+      <Route exact path="/" component={ NotFound } />
     </BrowserRouter>
   );
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
 
     this.state = {
+      loading: true,
       movies: [],
     };
+
+    console.log(movieAPI.getMovies());
+  }
+
+  componentDidMount() {
+    this.fetchApiMovies();
+  }
+
+  async fetchApiMovies() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const result = await movieAPI.getMovies();
+        this.setState({
+          loading: false,
+          movies: result,
+        });
+      },
+    );
   }
 
   render() {
-    const { movies } = this.state;
-
+    const { loading, movies } = this.state;
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { loading
+          ? <Loading />
+          : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </div>
     );
   }
